59b426043f95b61ac76dce91f15628e156a4a0db
Ai Nguyen <ainguyenkaka@gmail.com>
2018-04-10 16:26:47 +0800

    init

---
+++ README.md
@@ -0,0 +1,14 @@
+# StatNLP Server Monitor for Mobile
+
+## Development
+
+``` bash
+# install dependencies
+npm install
+
+# add platform
+cordova platform add browser
+
+# serve at localhost:8000
+cordova run browser
+```
---
+++ www/js/index.js
@@ -0,0 +1,12 @@
+var app = {
+    // Application Constructor
+    initialize: function() {
+        document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
+    },
+
+    onDeviceReady: function() {
+    },
+
+};
+
+app.initialize();
